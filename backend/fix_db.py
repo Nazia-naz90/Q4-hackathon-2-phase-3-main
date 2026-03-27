4d798d4fbf5f69570e9ab187890aae41328a9e4e
Nazia313 <nazianaznew313@gmail.com>
2026-03-28 02:52:50 +0500

    Removed all local files

--- backend/fix_db.py
+++
@@ -1,8 +0,0 @@
-from database import engine
-from sqlmodel import SQLModel
-from models import User, Task
-print('Dropping old tables in Neon...')
-SQLModel.metadata.drop_all(engine)
-print('Creating fresh tables with new columns...')
-SQLModel.metadata.create_all(engine)
-print('Database Synced Successfully!')
